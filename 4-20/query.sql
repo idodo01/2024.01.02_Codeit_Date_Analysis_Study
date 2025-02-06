563fd11d7e859a10d1946d1783c08a51e42c4ba7
IDO <susususu07@naver.com>
2025-02-06 14:43:59 +0900

    Update query.sql

--- 4-20/query.sql
+++ 4-20/query.sql
@@ -1,4 +1,4 @@
-SELECT category, main_month, COUNT(*) as "영화 수", SUM(view_count) as "총 관객 수"
+SELECT category, main_month, COUNT(*) as '영화 수', SUM(view_count) as '총 관객 수'
 FROM 2020_movie_report
 GROUP BY category, main_month
 HAVING main_month = 5 AND SUM(view_count) > 3000000
